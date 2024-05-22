4db9ed5e80eba53f38e1059772335b167eccebf7
arunnats <arun_b220019cs@nitc.ac.in>
2024-05-22 23:57:13 +0530

    ditched using a wrapper and instead using ytdl-core, works so far, need to ifgure out configuration options and file downloading

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const ejs = require("ejs");
 const path = require("path");
 const bodyParser = require("body-parser");
+const fs = require("fs");
+const ytdl = require("ytdl-core");
 
 const app = express();
 const port = 3000;
@@ -11,6 +13,33 @@ app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
+
+app.get("/download-video", async (req, res) => {
+	try {
+		const videoUrl = req.query.url;
+		if (!ytdl.validateURL(videoUrl)) {
+			return res.status(400).send("Invalid YouTube URL");
+		}
+
+		// Fetch video info to get the title
+		const info = await ytdl.getInfo(videoUrl);
+		const title = info.videoDetails.title;
+
+		// Create filename based on the video title
+		const filename = title.replace(/[^\w\s]/gi, "") + ".mp4";
+
+		// Pipe the video stream directly to a file with the video title as filename
+		ytdl(videoUrl).pipe(fs.createWriteStream(filename));
+
+		// Send a success response back to the client
+		res
+			.status(200)
+			.send(`Video download started successfully. Filename: ${filename}`);
+	} catch (error) {
+		console.error("Error downloading video:", error);
+		res.status(500).send("Error downloading video");
+	}
+});
 
 app.get("/", (req, res) => {
 	res.render("index");
